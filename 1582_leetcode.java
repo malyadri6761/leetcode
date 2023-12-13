2efefa29e40a669509c79019f4921b38025029b8
malyadri6761 <136675267+malyadri6761@users.noreply.github.com>
2023-12-13 08:19:51 +0530

    Create 1582_leetcode.java

---
+++ 1582_leetcode.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numSpecial(int[][] mat) {
+        int row[]=new int[mat.length];
+        int col[]=new int[mat[0].length];
+        for(int i=0;i<mat.length;i++){
+            for(int j=0;j<mat[0].length;j++){
+                if(mat[i][j]==1){
+                    ++row[i];
+                    ++col[j];
+                }
+            }
+                
+        }
+        int l=0;
+        for(int i=0;i<mat.length;i++){
+            for(int j=0;j<mat[0].length;j++){
+                if(mat[i][j]==1 && row[i]==1 && col[j]==1)
+                    ++l;
+            }
+        }
+        return l;
+    }
+    
+}
