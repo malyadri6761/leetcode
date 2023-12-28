29afb5749ddf39de0da48f8d40647e7c2b375eab
malyadri6761 <136675267+malyadri6761@users.noreply.github.com>
2023-12-28 19:26:47 +0530

    Create 434_leetcode.java

---
+++ 434_leetcode.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countSegments(String s) {
+       StringTokenizer st=new StringTokenizer(s," ");
+       int count=0;
+       while(st.hasMoreTokens()){
+           count++;
+           st.nextToken();
+       }
+       return count;
+    }
+}
